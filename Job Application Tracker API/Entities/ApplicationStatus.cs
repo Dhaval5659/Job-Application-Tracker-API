ff5842e374855df83c9cb5afafce8326a585e3a5
Dhaval Mangukiya <dhaval.enqcode@gmail.com>
2026-06-12 16:06:40 +0530

    Update to .NET 8.0 and add enums for status and roles
    Updated the project to target .NET 8.0 in the `.csproj` file.
    Removed the `Entities` folder reference from the `<ItemGroup>`
    section in the `.csproj` file. Added `ApplicationStatus` and
    `UserRole` enums in the `Job_Application_Tracker_API.Entities`
    namespace to define application statuses and user roles.

---
+++ Job Application Tracker API/Entities/ApplicationStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace Job_Application_Tracker_API.Entities
+{
+    public enum ApplicationStatus
+    {
+    Applied,
+    InterviewScheduled,
+    InterviewCompleted,
+    OfferReceived,
+    Rejected,
+    Withdrawn
+    }
+}
---
+++ Job Application Tracker API/Entities/UserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace Job_Application_Tracker_API.Entities
+{
+    public enum UserRole
+    {
+        Applicant,
+        Recruiter,
+        Admin
+    }
+}
