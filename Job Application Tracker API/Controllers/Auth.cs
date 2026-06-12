ed95e8e93acf30e700062b036a2068fa0bdfabdb
Dhaval Mangukiya <dhaval.enqcode@gmail.com>
2026-06-12 15:31:41 +0530

    Add project files.

---
+++ Job Application Tracker API/Controllers/Auth.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Job_Application_Tracker_API.Controllers
+{
+    public class Auth : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
